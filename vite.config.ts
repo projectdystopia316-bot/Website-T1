fd6387b42fd30146d642cbad4eebd54d269e7861
Dystopia <projectdystopia316@gmail.com>
2025-10-09 20:34:46 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/Wesbite-T1/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/Website-T1/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
